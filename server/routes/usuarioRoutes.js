1957575c34735992c136c857a5e318e42ac4fbdd
Gregory Veloso <gregory.v.silva@aluno.senai.br>
2026-05-27 13:35:44 -0300

    cadastro finalizado

--- server/routes/usuarioRoutes.js
+++ server/routes/usuarioRoutes.js
@@ -5,6 +5,12 @@ const router = express.Router();
 // Importar o controller do usuario
 const usuarioController = require("../controllers/usuarioController.js")
 
+// Importar o multer
+const upload = require("../config/multer.js")
+
+// Importar o middleware de autenticação
+const { verificarAutenticacao, somenteAdmin } = require("../middlewares/authMiddleware.js")
+
 // Declaração das rotas do usuário
 // ROTAS PÚBLICAS
 // Envia os dados de login
@@ -13,7 +19,14 @@ router.post("/login", usuarioController.login)
 // Rota de saida
 router.get("/logout", usuarioController.logout)
 
+// Rota de cadastro de usuário
+// O multer, salva a imagem primeiro, através do upload.single, depois chama o controller
+router.post('/cadastrar', upload.single('foto'), usuarioController.cadastrar )
+
 // ROTAS PRIVADAS
+// Daqui pra baixo, só executa se tiver acesso para tal
+router.use(verificarAutenticacao)
+router.use(somenteAdmin)
 
 // Obtém a lista de usuários
 router.get("/", (req, res) => {
